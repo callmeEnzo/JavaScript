c4def1e1e3bced50ebff7ff733caf34f3d163522
callmeEnzo <lorenzo.bautista@adamson.edu.ph>
2026-05-15 20:35:44 +0800

    Add files via upload

---
+++ Bautista/Lab Activity/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main()
+    {
+        // create a stack
+        Stack<string> subject = new Stack<string>();
+        subject.Push("Math");
+        subject.Push("English");
+
+        // pop the element from top of stack
+        Console.WriteLine("Popped element: " + subject.Pop());
+    }
+}
